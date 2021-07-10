1eb31d4e2bdedda321bd2f3f9c348c7c5075a990
Stu Doonan <studoonan@yahoo.co.uk>
2021-07-10 16:43:16 +0100

    updated services and controllers

--- app/Services/TMDB/MovieService.php
+++ app/Services/TMDB/MovieService.php
@@ -1,24 +1,20 @@
 <?php
+
 namespace App\Services;
 
-use GuzzleHttp\Client;
-use Illuminate\Http\Response;
-use App\Exceptions\ApiException;
-use App\Exceptions\ServerException;
 use App\Exceptions\NotFoundException;
-use App\Contracts\RestApiServiceContract;
+use App\Exceptions\TmdbException;
+use App\Services\RestApiService;
 use GuzzleHttp\Exception\RequestException;
 
-class RestApiService implements RestApiServiceContract
+class MovieService extends RestApiService
 {
-    protected $client;
 
     /**
-     * Class constructor.
+     * MovieService constructor.
      */
-    public function __construct(Client $client)
+    public function __construct()
     {
-        $this->client = $client;
     }
 
     /**
@@ -199,85 +195,5 @@ class RestApiService implements RestApiServiceContract
             }
         }
     }
-
-
-    /**
-     * Returns a configuration object
-     *
-     * @return array
-     */
-    public function getConfiguration(): array
-    {
-        try {
-            $response = $this->client->request('GET', '/3/configuration');
-            return json_decode($response->getBody()->getContents(), true);
-        } catch (RequestException $exception) {
-            switch ($exception->getResponse()->getStatusCode()) {
-                case 404:
-                    throw new NotFoundException($exception->getMessage());
-                    break;
-                case 500:
-                    throw new ServerException($exception->getMessage());
-                    break;
-                default:
-                    throw new ApiException($exception->getMessage());
-                    break;
-            }
-        }
-    }
-
-    /**
-     * Returns the trailer associated with a movie
-     *
-     * @param $id
-     * @return mixed
-     */
-    public function getTrailer($id)
-    {
-        try {
-            $response = $this->client->request('GET', '/3/movie/' . $id . '/videos');
-            return json_decode($response->getBody()->getContents(), true);
-        } catch (RequestException $exception) {
-            switch ($exception->getResponse()->getStatusCode()) {
-                case 404:
-                    throw new NotFoundException($exception->getMessage());
-                    break;
-                case 500:
-                    throw new ServerException($exception->getMessage());
-                    break;
-                default:
-                    throw new ApiException($exception->getMessage());
-                    break;
-            }
-        }
-    }
-
-    /**
-     * Returns a configuration object
-     *
-     * @param $id
-     * @return Response
-     */
-    public function createRating($id): Response
-    {
-        try {
-            $response = $this->client->post('/3/movie/' . $id . '/rating');
-            return json_decode($response->getBody()->getContents(), true);
-        } catch (RequestException $exception) {
-            switch ($exception->getResponse()->getStatusCode()) {
-                case 404:
-                    throw new NotFoundException($exception->getMessage());
-                    break;
-                case 500:
-                    throw new ServerException($exception->getMessage());
-                    break;
-                default:
-                    throw new ApiException($exception->getMessage());
-                    break;
-            }
-        }
-    }
-
-
 
 }
