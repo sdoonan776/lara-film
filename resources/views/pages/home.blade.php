a7081a76ec3b6fd27571cc4466b548750d315569
Stu Doonan <studo@pop-os.localdomain>
2021-07-25 23:40:04 +0100

    updated homepage styles

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -42,29 +42,28 @@
             <section class="movie-section">
                 <div class="container now-playing">
                     <h6>Now Playing</h6>
-                    <div class="owl-carousel owl-theme">
-                        @foreach ($recentMovies as $movie)
-                            <div class="movie-image-container col-lg-3">
-                                <div class="movie-image" style="width: 12em;">
-                                    <a href="{{ route('movie.show', ['id' => $movie['id']]) }}">
-                                        <img src="{{ $imageUrl }}/{{ $posterImageSize }}{{ $movie['poster_path'] }}" alt="{{ $movie['original_title'] }}">
-                                    </a>
-                                </div>
+                    @foreach ($recentMovies as $movie)
+                        <div class="movie-image-container col-lg-3">
+                            <div class="movie-image" style="width: 12em;">
+                                <a href="{{ route('movie.show', ['id' => $movie['id']]) }}">
+                                    <img src="{{ $imageUrl }}/{{ $posterImageSize }}{{ $movie['poster_path'] }}" alt="{{ $movie['original_title'] }}">
+                                </a>
                             </div>
-                        @endforeach
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
             </section>
         @else
             <div class="container">
-                <h1>No Films Available</h1>
-                <span>Please try again later</span>
+                @foreach(range(1, 5) as $movie)
+                    <div class="no-movie-image-container"></div>
+                @endforeach
             </div>
         @endif
 
-        @if (count($topRatedMovies) > 0)
+        @if (isset($topRatedMovies))
             <section class="movie-section">
-                <div class="container now-playing">
+                <div class="container top-rated">
                     <h6>Top Rated</h6>
                     <div class="row">
                         @foreach ($topRatedMovies as $movie)
@@ -77,46 +76,41 @@
                             </div>
                         @endforeach
                     </div>
-
                 </div>
             </section>
         @else
             <div class="container">
-                <h1>No Films Available</h1>
-                <span>Please try again later</span>
+                @foreach(range(1, 5) as $movie)
+                    <div class="no-movie-image-container"></div>
+                @endforeach
             </div>
         @endif
 
-        @if (count($upcomingMovies) > 0)
+        @if (isset($upcomingMovies))
             <section class="movie-section">
-                <div class="container now-playing">
+                <div class="container upcoming">
                     <h6>Upcoming</h6>
-                    <div class="owl-carousel owl-theme">
-                        @foreach ($upcomingMovies as $movie)
-                            <div class="movie-image-container col-lg-3">
-                                <div class="movie-image" style="width: 12em;">
-                                    <a href="{{ route('movie.show', ['id' => $movie['id']]) }}">
-                                        <img src="{{ $imageUrl }}/{{ $posterImageSize }}{{ $movie['poster_path'] }}" alt="{{ $movie['original_title'] }}">
-                                    </a>
-                                </div>
+                    @foreach ($upcomingMovies as $movie)
+                        <div class="movie-image-container col-lg-3">
+                            <div class="movie-image" style="width: 12em;">
+                                <a href="{{ route('movie.show', ['id' => $movie['id']]) }}">
+                                    <img src="{{ $imageUrl }}/{{ $posterImageSize }}{{ $movie['poster_path'] }}" alt="{{ $movie['original_title'] }}">
+                                </a>
                             </div>
-                        @endforeach
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
             </section>
         @else
             <div class="container">
-                <h1>No Films Available</h1>
-                <span>Please try again later</span>
+                @foreach(range(1, 5) as $movie)
+                    <div class="no-movie-image-container"></div>
+                @endforeach
             </div>
         @endif
 
 	</main>
 @endsection
-{{--<script--}}
-{{--    src="https://code.jquery.com/jquery-3.6.0.min.js"--}}
-{{--    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="--}}
-{{--    crossorigin="anonymous"></script>--}}
 <script>
     $(document).ready(function(){
         $('.owl-carousel').owlCarousel({
@@ -134,18 +128,5 @@
                 }
             }
         });
-
-
-{{--        // for(let j = 0; j < 5; j++) {--}}
-{{--        //     $('<div class="carousel-item"><img--}}
-{{--        //     src="https://loremflickr.com/320/240" width="50%">--}}
-{{--        //         </div>').appendTo('.carousel-inner');--}}
-{{--        //     $('<li data-target="#carousel" data-slide-to="'+j+'">--}}
-{{--        //         </li>').appendTo('.carousel-indicators')--}}
-{{--        // }--}}
-
-{{--        $('.carousel-item').first().addClass('active');--}}
-{{--        $('.carousel-indicators > li').first().addClass('active');--}}
-{{--        $('#carousel').carousel();--}}
     });
 </script>
